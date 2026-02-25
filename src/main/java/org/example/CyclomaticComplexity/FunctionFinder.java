fa189d6284b0b2af421d13415f29b0f6d3c51b30
nmthaiss <c3440776@uon.edu.au>
2026-02-26 10:20:15 +1100

    added some stuff but this branch should be gone

---
+++ src/main/java/org/example/CyclomaticComplexity/FunctionFinder.java
@@ -0,0 +1,20 @@
+package org.example.CyclomaticComplexity;
+
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.GenericListVisitorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunctionFinder extends GenericListVisitorAdapter<MethodDeclaration, Void> {
+    @Override
+    public List<MethodDeclaration> visit(MethodDeclaration n, Void args) {
+        super.visit(n, args);
+        List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
+        if (n.getBody().isPresent()) {
+            methods.add(n);
+        }
+
+        return methods;
+    }
+}
