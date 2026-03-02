7bd30f112416900ce9f3150cdc16002a1fe064db
nmthaiss <c3440776@uon.edu.au>
2026-03-02 14:58:01 +1100

    added core cyclomatic complexity logic and test

---
+++ src/main/test/CCTest.java
@@ -0,0 +1,23 @@
+import org.example.CyclomaticComplexity.CyclomaticComplexity;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import org.example.JavaParserProvider;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.example.Main.parseProject;
+
+public class CCTest {
+    @Test
+    public void testCC() throws IOException {
+        Path projectPath = Path.of("src/main/resources/powsybl-open-loadflow/src");
+        JavaParserProvider.initialization(projectPath);
+        List<ParseResult<CompilationUnit>> parsedFiles = parseProject();
+        CyclomaticComplexity cc = new CyclomaticComplexity();
+        cc.caclCyclomaticComplexity(parsedFiles);
+    }
+
+}
