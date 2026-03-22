09546c9f8e91f1adf874d654d2c0f9dcf3f6a140
James Tolputt <james.tolputt1@gmail.com>
2026-03-22 16:31:04 +1100

    feat: cc cyclomaticComplexity metric added

--- src/main/java/org/example/commands/CyclomaticComplexityCommand.java
+++ src/main/java/org/example/commands/CyclomaticComplexityCommand.java
@@ -1,4 +1,129 @@
 package org.example.commands;
 
-public class CyclomaticComplexityCommand {
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.SourceRoot;
+import org.example.JavaParserProvider;
+import org.example.QualityToolCLI;
+import org.example.services.CyclomaticComplexity.CyclomaticComplexity;
+import org.example.services.CyclomaticComplexity.CyclomaticComplexityResult;
+import org.example.utils.Console;
+import picocli.CommandLine.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@Command(
+        name = "cc",
+        aliases = {},
+        mixinStandardHelpOptions = true,
+        version = "qualitytool 1.0",
+        description = {
+                "Cyclomatic Complexity Analysis"
+        },
+        footer = {
+            "",
+            "  @|bold Examples:|@",
+            "    qualitytool cc -p ./project --no-color",
+            "    qualitytool cc -p ./project -t -T 20 -W 10",
+            ""
+        }
+)
+public class CyclomaticComplexityCommand extends BaseMetricCommand {
+    @ParentCommand
+    QualityToolCLI parent;
+
+    @Override
+    public String displayName() {
+        return "Cyclomatic Complexity Analysis";
+    }
+
+    @Option(names = {"-T", "--threshold"},
+            description = "Threshold before cyclomatic complexity fails",
+            defaultValue = "20"
+    )
+    String failureThreshold;
+
+    @Option(names = {"-W", "--warning"},
+            description = "Threshold before cyclomatic complexity is considered a warning",
+            defaultValue = "10"
+    )
+    String warningThreshold;
+
+    boolean exceededThresholdResult = false;
+
+
+    @Override
+    public Integer execute() throws Exception {
+        int warningThresholdNumber = 0;
+        int failureThresholdNumber = 0;
+        try {
+            warningThresholdNumber = Integer.parseInt(warningThreshold);
+            failureThresholdNumber = Integer.parseInt(failureThreshold);
+        } catch (NumberFormatException e) {
+            Console.error("Threshold numbers must be a integers");
+            return 1;
+        }
+
+        List<ParseResult<CompilationUnit>> parsedFiles;
+//        try {
+            parsedFiles = parseProject();
+            if (parsedFiles.isEmpty()) {
+                Console.error("Project path contains no java files!");
+                return 1;
+            }
+//        } catch (IOException e) {
+//
+//        }
+
+        CyclomaticComplexity cc = new CyclomaticComplexity();
+        String[] headers = {
+                "File",
+                "Average",
+                "Highest Score",
+                "Severity"
+        };
+
+        int[] widths = {
+                Console.terminalWidth() - 31,
+                8,
+                14,
+                9
+        };
+
+        boolean hasFailed = false;
+        List<CyclomaticComplexityResult> result = cc.calculateCyclomaticComplexity(parsedFiles, warningThresholdNumber, failureThresholdNumber);
+        String[][] rows = new String[result.size()][headers.length];
+        int rowIndex = 0;
+        for (CyclomaticComplexityResult cyclomaticComplexityResult : result) {
+            rows[rowIndex][0] = cyclomaticComplexityResult.getFileName();
+            rows[rowIndex][1] = "%.1f".formatted(cyclomaticComplexityResult.getAverageScore());
+            rows[rowIndex][2] = String.valueOf(cyclomaticComplexityResult.getHighestScore());
+
+            switch(cyclomaticComplexityResult.getSeverity()) {
+                case INFO -> rows[rowIndex][3] = Console.cyan("INFO");
+                case WARNING -> rows[rowIndex][3] = Console.yellow("WARNING");
+                case ERROR -> {
+                    rows[rowIndex][3] = Console.boldRed("ERROR");
+                    exceededThresholdResult = true;
+                }
+            }
+
+            rowIndex++;
+        }
+
+        System.out.println(Console.table(headers, widths, rows));
+
+        return 0;
+    }
+
+    @Override
+    public boolean exceededThreshold(Object result) {
+        return exceededThresholdResult;
+    }
+
+    public static List<ParseResult<CompilationUnit>> parseProject() throws IOException {
+        SourceRoot parser = JavaParserProvider.getInstance();
+        return parser.tryToParse();
+    }
 }
--- src/main/java/org/example/services/CyclomaticComplexity/CyclomaticComplexity.java
+++ src/main/java/org/example/services/CyclomaticComplexity/CyclomaticComplexity.java
@@ -4,6 +4,7 @@ import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -25,5 +26,57 @@ public class CyclomaticComplexity {
             }
         });
 //        return atomicInteger.get();
+    }
+
+    public List<CyclomaticComplexityResult> calculateCyclomaticComplexity(List<ParseResult<CompilationUnit>> parsedFiles, int warning, int severe){
+        List<CyclomaticComplexityResult> cyclomaticComplexityList = new ArrayList<>();
+
+        parsedFiles.forEach(result -> {
+            if (result.isSuccessful() && result.getResult().isPresent()) {
+                CompilationUnit cU = result.getResult().get();
+
+                String filename = cU.getStorage()
+                        .map(CompilationUnit.Storage::getFileName)
+                        .orElse("Unknown File");
+
+                double averageScore = 0.;
+                int highestScore = 0;
+                String highestScoreString = "";
+                CyclomaticComplexityResult.Severity severity = CyclomaticComplexityResult.Severity.ERROR;
+
+                List<MethodDeclaration> list = cU.accept(new FunctionFinder(), null);
+                for (MethodDeclaration method : list){
+                    AtomicInteger atomicInteger = new AtomicInteger(0);
+                    method.accept(new ComplexityVisitor(), atomicInteger);
+
+                    if (atomicInteger.get() > highestScore) {
+                        highestScore = atomicInteger.get();
+                        highestScoreString = method.getNameAsString();
+                    }
+
+                    averageScore += atomicInteger.get();
+                }
+
+                averageScore /= list.size();
+
+                if (highestScore >= warning && highestScore < severe) {
+                    severity = CyclomaticComplexityResult.Severity.WARNING;
+                } else if (highestScore < warning) {
+                    severity = CyclomaticComplexityResult.Severity.INFO;
+                }
+
+                CyclomaticComplexityResult unitResult = new CyclomaticComplexityResult(
+                        filename,
+                        averageScore,
+                        highestScore,
+                        highestScoreString,
+                        severity
+                        );
+
+                cyclomaticComplexityList.add(unitResult);
+            }
+        });
+
+        return cyclomaticComplexityList;
     }
 }
