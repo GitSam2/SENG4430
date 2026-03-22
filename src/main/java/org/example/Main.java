ed50aa1754cfa308b5f87ce709c9a45af6da7da2
JamesTolputt <james.tolputt1@gmail.com>
2026-03-23 09:38:04 +1100

    chore: remove unneeded function and imports in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ import picocli.CommandLine;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static MetricContext ctx;
@@ -25,37 +24,5 @@ public class Main {
     public static List<ParseResult<CompilationUnit>> parseProject() throws IOException {
         SourceRoot parser = JavaParserProvider.getInstance();
         return parser.tryToParse();
-    } // I think this would mean that the project as re-passed every time parseProject() is used.
-
-    public void metricSelection(Scanner scanner) throws IOException {
-//        if (projectPath == null) {                        // prolly need to make a project path check bcs what if they feed dumb path
-//            // either exit or attempt to reread this
-//            System.out.println("ProjectPath is null");
-//            return;
-//        }
-//
-//        if (!projectPath.startsWith("src")) {           // can trace back to src root from one of the path library using the current path
-//            System.out.println("ProjectPath is not source root");
-//            return;
-//        }
-//        System.out.println("ProjectPath is " + projectPath);
-        System.out.println("Welcome to watevs this thing is, please choose metric,...etc, input n1 for Cyclomatic complexity, n2 for, n3 for,..., -1 to exit");
-        int userInput = scanner.nextInt();
-
-        while (userInput != -1) {           // -1 = exit program
-            switch (userInput) {
-                case 1:
-                    List<ParseResult<CompilationUnit>> parsedFiles = parseProject();
-                    // call metric method from its class here
-                    break;
-                case 2:
-                    // whatevs your metric call is idk
-                default:
-                    System.out.println("Welcome to watevs this thing is, please choose metric,...etc, input n1 for Cyclomatic complexity, n2 for, n3 for,..., -1 to exit");
-                    break;
-            }
-            userInput = scanner.nextInt();
-        }
-
     }
 }
