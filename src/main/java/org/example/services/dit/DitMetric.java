70629f73c3e4f2f6b1a142ad0652e99998eb8352
Mackenzie Roddenby <mackenzieroddenby@gmail.com>
2026-03-29 14:25:48 +1100

    fix: added caching to DIT algorithm for improved mem management

--- src/main/java/org/example/services/dit/DitMetric.java
+++ src/main/java/org/example/services/dit/DitMetric.java
@@ -1,44 +1,62 @@
 package org.example.services.dit;
 
-import com.github.javaparser.ast.CompilationUnit;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.example.services.Metric;
 import org.example.services.MetricContext;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.github.javaparser.ast.CompilationUnit;
 
 /*
  * This class is uses for getting the Depth of Inheritance Tree (DIT)
  * Definition: DIT is the maximum length from the node (class) to the root of the tree.
  */
 public final class DitMetric implements Metric<DitResult> {
-    @Override public String id() { return "DIT"; }
+    @Override
+    public String id() {
+        return "DIT";
+    }
 
     @Override
     public DitResult compute(MetricContext ctx) {
         // Get inheritance
-        Map<String, String> inheritanceMap = new HashMap<>();
-        InheritanceCollector collector = new InheritanceCollector();
+        Map<String, String> inheritanceMap = new HashMap<>(); // key is class name and value is parent class name
+        InheritanceCollector collector = new InheritanceCollector(); // visitor pattern
+                                                                     // https://refactoring.guru/design-patterns/visitor
         for (CompilationUnit cu : ctx.compilationUnits()) {
-            cu.accept(collector, inheritanceMap);
+            cu.accept(collector, inheritanceMap); // visits each class and puts name and parent in inheritance map
         }
 
         // compute DIT per class
         Map<String, Integer> ditByClass = new HashMap<>();
+        Map<String, Integer> cache = new HashMap<>();
 
         for (String className : inheritanceMap.keySet()) {
-            int dit = computeDit(className, inheritanceMap);
+            int dit = computeDit(className, inheritanceMap, cache);
             ditByClass.put(className, dit);
         }
         return new DitResult(ditByClass, 4, 5);
     }
 
-    private int computeDit(String className, Map<String, String> inheritanceMap) {
-        // todo... possible improvements with caching here
-        String parent = inheritanceMap.get(className);
-        if (parent == null) {
-            return 0;
+    private int computeDit(String className, Map<String, String> inheritanceMap, Map<String, Integer> cache) {
+        int dit;
+        // check if DIT for this class has already been computed, return if it has been
+        if (cache.containsKey(className)) {
+            return cache.get(className);
         }
-        return 1 + computeDit(parent, inheritanceMap);
+
+        String parent = inheritanceMap.get(className);
+
+        // if parent is null then we are at the root
+        if (parent == null) {
+            dit = 0;
+        } else { // otherwise, DIT is 1 + DIT of parent
+            dit = 1 + computeDit(parent, inheritanceMap, cache);
+        }
+
+        // store DIT in cache to stop recomputing
+        cache.put(className, dit);
+        return dit;
     }
 }
--- src/main/java/org/example/services/dit/InheritanceCollector.java
+++ src/main/java/org/example/services/dit/InheritanceCollector.java
@@ -1,17 +1,25 @@
 package org.example.services.dit;
 
+import java.util.Map;
+
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-import java.util.Map;
-
+/*
+    * This class extends the VoidVisitorAdapter, getting default
+    * implementations of visit methods. This allows us to get details
+    * of the visit (in this case, class name and parent class name) 
+    * and store it in the inheritance map.
+ */
 public final class InheritanceCollector
         extends VoidVisitorAdapter<Map<String, String>> {
 
     @Override
     public void visit(ClassOrInterfaceDeclaration decl, Map<String, String> inheritanceMap) {
+        // perform actual visit using cu visit method
         super.visit(decl, inheritanceMap);
 
+        // get details of the visit for the inheritance map
         String className = decl.getNameAsString();
 
         if (decl.getExtendedTypes().isNonEmpty()) {
